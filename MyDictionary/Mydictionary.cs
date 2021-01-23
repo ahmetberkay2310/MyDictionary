0559616e2b70b172ad5768ae457e4fbe0a95937a
Berkay <Berkay@DESKTOP-GPUTPPI>
2021-01-23 20:41:54 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Mydictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] Markalar;
+        public MyDictionary()
+        {
+            Markalar = new T[0];
+        }
+
+        public void Add (T marka)
+        {
+            T[] Keep = Markalar;
+            Markalar = new T[Markalar.Length];
+            for (int i = 0; i < Keep.Length; i++)
+
+            {
+                Markalar[i] = Keep[i];
+            }
+            Markalar[Markalar.Length - 1] = marka;
+
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> ArabaMarkası = new MyDictionary<string>();
+            ArabaMarkası.Add("Seat");
+            ArabaMarkası.Add("Skoda");
+            ArabaMarkası.Add("bmw");
+        }
+    }
+}
